411ba764f3da0eac453c3309ae8a2b3848af9464
mindwm <metacoma@github.com>
2024-07-26 09:59:51 +0000

    See https://github.com/mindwm/mindwm-api/commit/b3c91beb8a2d280ab7d8d0fc5f10173c2e84ed64 from refs/heads/master

--- MindWM/models/__init__.py
+++ MindWM/models/__init__.py
@@ -19,4 +19,6 @@ from MindWM.models.clipboard_context import ClipboardContext
 from MindWM.models.clipboard_event import ClipboardEvent
 from MindWM.models.io_document import IoDocument
 from MindWM.models.io_document_event import IoDocumentEvent
+from MindWM.models.touch import Touch
+from MindWM.models.touch_event import TouchEvent
 from MindWM.models.vector2int import Vector2int
