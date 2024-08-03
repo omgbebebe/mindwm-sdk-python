c78b22d1616813ebc13e850f36f21d1401656109
Sergey Bubnov <omgbebebe@gmail.com>
2024-08-03 19:59:29 +0400

    make a real module for mindwm.neomodel

---
+++ src/mindwm/neomodel/__init__.py
@@ -0,0 +1,7 @@
+from mindwm.neomodel.neomodel_data import MindwmUser
+from mindwm.neomodel.neomodel_data import MindwmHost
+from mindwm.neomodel.neomodel_data import Tmux
+from mindwm.neomodel.neomodel_data import TmuxSession
+from mindwm.neomodel.neomodel_data import TmuxPane
+from mindwm.neomodel.neomodel_data import IoDocument
+from mindwm.neomodel.neomodel_data import ClipBoard
